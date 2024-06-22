3409252f0b0ed4a7cb14a6e7790c1607711fdb27
VargasGandolini <avg072023@gmail.com>
2024-06-21 23:57:28 -0400

    Tipos de Datos

---
+++ JavaScript/01-tipos/05-comentarios.js
@@ -0,0 +1,14 @@
+// hola
+/*
+Todo lo que escribamos
+entre medio de los slash
+va a ser ignorado
+*/
+/**
+ * Comentario
+ * en
+ * Bloque
+ */
+let numero = 42; // Este es otro comentario
+
+console.log(numero)
