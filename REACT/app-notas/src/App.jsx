5aff3d0220824f754c12a67187fddd632c25d34c
VargasGandolini <avg072023@gmail.com>
2024-06-24 19:43:34 -0400

    Crear Notas

--- REACT/app-notas/src/App.jsx
+++ REACT/app-notas/src/App.jsx
@@ -1,11 +1,43 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [nueva, setNueva] = useState(false);
+  const [nota, setNota] = useState("");
+  const [notas, setNotas] = useState([]);
+
+  function nuevaNota() {
+    setNueva(!nueva);
+  }
+
+  function guardarNota() {
+    setNotas([...notas, nota]);
+    setNueva(!nueva);
+  }
   return (
     <>
       <header>
         <h1> Notas </h1>
       </header>
+      <main>
+        <button className="nueva" onClick={nuevaNota} />
+        {nueva && (
+          <section>
+            <textarea
+              rows="15"
+              cols="50"
+              placeholder="Nueva Nota"
+              value={nota}
+              onChange={(e) => setNota(e.target.value)}
+            ></textarea>
+            <button className="guardar" onClick={guardarNota}>
+              {" "}
+              GUARDAR NOTA{" "}
+            </button>
+          </section>
+        )}
+        {notas}
+      </main>
     </>
   );
 }
